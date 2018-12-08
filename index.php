ae001ac2b8ef227c7a1b18fc6d5f1589c14380b2
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-12-08 20:50:01 -0300

    PROJETO BASE inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>READNE.md Generator</title>
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+
+</div>
+</body>
+<script src="/js/bootstrap.min.js"></script>
+</html>
